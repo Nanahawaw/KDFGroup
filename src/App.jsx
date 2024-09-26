0422adad785b6f20e8a214e731e1d5158905421b
glowwEE <gloryokafor5@gmail.com>
2024-09-26 22:13:41 +0100

    first commit and added few dependencies

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='mt-44 bg-purple-400 p-24 mx-40 rounded-3xl border border-purple-400 shadow-2xl transition-all hover:translate-y-8 hover:bg-purple-100 hover:border-purple-400 hover:text-purple-400'>
+      <h1 className='text-6xl text-purple-900 text-center font-bold'>🎉🎉Welcome to KDFG🥳🎉 <span className="font-mono  text-purple-200 hover:text-purple-400">hwan-yeong</span></h1>
+    </div>
+  )
+}
+
+export default App
