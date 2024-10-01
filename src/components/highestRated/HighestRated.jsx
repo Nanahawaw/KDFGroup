73a57c1f3758a198b2b3dfd7026c5efe4a55d221
Diana <mada.chirica@gmail.com>
2024-10-01 14:24:30 +0300

    frontend - highest rated images work. Full copy as it was

--- src/components/highestRated/HighestRated.jsx
+++ src/components/highestRated/HighestRated.jsx
@@ -1,4 +1,3 @@
-// eslint-disable-next-line react/prop-types
 const HighestRated = ({ titles }) => {
   return (
     <div>
--- src/components/highestRated/titles.jsx
+++ src/components/highestRated/titles.jsx
@@ -1,21 +1,53 @@
+// Import the SVG files directly
+import lovelyRunner from "../../assets/drama_posters/lovely_runner.svg"
+import marryMyHusband from "../../assets/drama_posters/marry_my_husband.svg"
+import vincenzo from "../../assets/drama_posters/vincenzo.svg"
+import alchemy from "../../assets/drama_posters/alchemy.svg"
+import iotnbo from "../../assets/drama_posters/iotnbo.svg"
+import myDemon from "../../assets/drama_posters/my_demon.svg"
+
+// Create an array with imported images
 const highestRatedTitles = [
   {
-    image: "/drama_posters/lovely_runner.svg",
+    image: lovelyRunner,
   },
   {
-    image: "/drama_posters/marry_my_husband.svg",
+    image: marryMyHusband,
   },
   {
-    image: "/drama_posters/vincenzo.svg",
+    image: vincenzo,
   },
   {
-    image: "/drama_posters/alchemy.svg",
+    image: alchemy,
   },
   {
-    image: "/drama_posters/iotnbo.svg",
+    image: iotnbo,
   },
   {
-    image: "/drama_posters/my_demon.svg",
+    image: myDemon,
   },
 ]
+
 export default highestRatedTitles
+
+// const highestRatedTitles = [
+//   {
+//     image: "/drama_posters/lovely_runner.svg",
+//   },
+//   {
+//     image: "/drama_posters/marry_my_husband.svg",
+//   },
+//   {
+//     image: "/drama_posters/vincenzo.svg",
+//   },
+//   {
+//     image: "/drama_posters/alchemy.svg",
+//   },
+//   {
+//     image: "/drama_posters/iotnbo.svg",
+//   },
+//   {
+//     image: "/drama_posters/my_demon.svg",
+//   },
+// ]
+// export default highestRatedTitles
