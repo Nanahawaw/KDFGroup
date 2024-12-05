0d1ed7a3fb4ccebd0dc78430c102fe76a543bd0b
Diana <mada.chirica@gmail.com>
2024-12-05 20:03:25 +0000

    movie catalogue images and props

---
+++ src/components/movieCatalogue/movieCatalogueFakeProps.jsx
@@ -0,0 +1,129 @@
+import image1 from "../../assets/movieCatalogue/queen-of-tears.png";
+import image2 from "../../assets/movieCatalogue/a-killer-paradox.png";
+import image3 from "../../assets/movieCatalogue/welcome-to-samdal-ri.png";
+import image4 from "../../assets/movieCatalogue/connection.png";
+import image5 from "../../assets/movieCatalogue/perfect-marriage-revenge.png";
+import image6 from "../../assets/movieCatalogue/kind-the-land.png";
+import image7 from "../../assets/movieCatalogue/marry-my-husband.png";
+import image8 from "../../assets/movieCatalogue/dare-to-love-me.png";
+import image9 from "../../assets/movieCatalogue/the-impossible-heir.png";
+import image10 from "../../assets/movieCatalogue/pyramid-game.png";
+import image11 from "../../assets/movieCatalogue/the-midnight-romance-in-hagwon.png";
+import image12 from "../../assets/movieCatalogue/doctor-slump.png";
+import image13 from "../../assets/movieCatalogue/dreaming-of-a-freaking-fairy-tale.png";
+import image14 from "../../assets/movieCatalogue/the-escape-of-the-seven.png";
+import image15 from "../../assets/movieCatalogue/a-shop-for-killers.png";
+import image16 from "../../assets/movieCatalogue/flex-x-cop.png";
+import image17 from "../../assets/movieCatalogue/the-uncanny-counter.png";
+import image18 from "../../assets/movieCatalogue/my-demon.png";
+import image19 from "../../assets/movieCatalogue/the-glory.png";
+import image20 from "../../assets/movieCatalogue/penthouse.png";
+import image21 from "../../assets/movieCatalogue/mr-queen.png";
+
+export const dramasCatalogue = [
+  {
+    id: "1",
+    image: image1,
+    title: "Queen of Tears",
+  },
+  {
+    id: "2",
+    image: image2,
+    title: "A Killer Paradox",
+  },
+  {
+    id: "3",
+    image: image3,
+    title: "Welcome to Samdal-ri",
+  },
+  {
+    id: "4",
+    image: image4,
+    title: "Connection",
+  },
+  {
+    id: "5",
+    image: image5,
+    title: "Perfect Marriage Revenge",
+  },
+  {
+    id: "6",
+    image: image6,
+    title: "King the Land",
+  },
+  {
+    id: "7",
+    image: image7,
+    title: "Marry my Husband",
+  },
+  {
+    id: "8",
+    image: image8,
+    title: "Dare to Love me",
+  },
+  {
+    id: "9",
+    image: image9,
+    title: "The Impossible Heir",
+  },
+  {
+    id: "10",
+    image: image10,
+    title: "Pyramid Game",
+  },
+  {
+    id: "11",
+    image: image11,
+    title: "The Midnight Romance in Hagwon",
+  },
+  {
+    id: "12",
+    image: image12,
+    title: "Doctor Slump",
+  },
+  {
+    id: "13",
+    image: image13,
+    title: "Dreaming of a Freaking Fairy Tale",
+  },
+  {
+    id: "14",
+    image: image14,
+    title: "The Escape of the Seven",
+  },
+  {
+    id: "15",
+    image: image15,
+    title: "A Shop for Killers",
+  },
+  {
+    id: "16",
+    image: image16,
+    title: "Flex X Cop",
+  },
+  {
+    id: "17",
+    image: image17,
+    title: "The Uncanny Counter",
+  },
+  {
+    id: "18",
+    image: image18,
+    title: "My Demon",
+  },
+  {
+    id: "19",
+    image: image19,
+    title: "The Glory",
+  },
+  {
+    id: "20",
+    image: image20,
+    title: "Penthouse",
+  },
+  {
+    id: "21",
+    image: image21,
+    title: "Mr Queen",
+  },
+];
